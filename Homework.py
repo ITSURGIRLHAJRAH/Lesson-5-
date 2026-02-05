a81a6cf36b24fa69ad8150fc474a10ed010de799
Hajrah Javaid <saadi.hajrajavaid@gmail.com>
2026-02-05 21:52:12 +0500

    Add files via upload

---
+++ Homework.py
@@ -0,0 +1,10 @@
+season=input("Enter season:Summer/Rainy/Winter: ")
+
+if season=="Summer":
+    print("You can wear cotton clothes")
+
+elif season=="Rainy":
+    print("You can wear a raincoat or bathe in the rain if ur cool")
+
+else:
+    print("You can wear BIG clothes to not get freezed")
